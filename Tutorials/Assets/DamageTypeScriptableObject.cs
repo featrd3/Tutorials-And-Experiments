d7243b35eb58f308a04cd7bb8066f0809d70b742
featrd3 <43852603+featrd3@users.noreply.github.com>
2021-07-21 17:57:40 +0200

    Preparing simple scene, creating simple scribtable objects

---
+++ Tutorials/Assets/DamageTypeScriptableObject.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/DamageTypeScriptableObject", order = 1)]
+public class DamageTypeScriptableObject : ScriptableObject
+{
+    public string damageName;
+
+    public Color colour;
+}
---
+++ Tutorials/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] private TMP_Text damageTextPrefab = null;
+    [SerializeField] private DamageTypeScriptableObject[] damageTypes = new DamageTypeScriptableObject[0];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Input.GetKeyDown(KeyCode.Space)) { return; }
+        DealDamage();
+    }
+
+    private void DealDamage()
+    {
+        int damageToDeal = Random.Range(1, 11);
+        DamageTypeScriptableObject damageType = damageTypes[Random.Range(0, damageTypes.Length)];
+
+        SpawnDamageText(damageToDeal, damageType);
+    }
+    private void SpawnDamageText(int damageToDeal, DamageTypeScriptableObject damageType)
+    {
+        TMP_Text damageTextInstance = Instantiate(damageTextPrefab, transform.position, Quaternion.identity);
+
+        damageTextInstance.text = damageToDeal.ToString();
+        damageTextInstance.color = damageType.colour;
+
+        Destroy(damageTextInstance.gameObject, 1f);
+    }
+}
