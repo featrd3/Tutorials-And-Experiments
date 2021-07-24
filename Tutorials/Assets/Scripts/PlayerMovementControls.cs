1b0365e6366c9ea89ebd0918772e611d290704d6
featrd3 <43852603+featrd3@users.noreply.github.com>
2021-07-24 18:04:49 +0200

    Added player controls for movement, jump and camera controls.

---
+++ Tutorials/Assets/Scripts/PlayerMovementControls.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementControls : MonoBehaviour
+{
+
+    public CharacterController controller;
+    public float speed = 12f;
+    Vector3 velocity;
+
+    public float jumpForce = 10f;
+    public float gravity = -9.81f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0){
+            velocity.y = -2f;
+           if(Input.GetButtonDown("Jump"))
+            {
+                velocity.y = jumpForce;
+            }
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime ;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
